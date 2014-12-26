1feddadd37ca3451f5a97bc3f458ea90fca88197
kashiha <snow.tech29@gmail.com>
2014-12-26 13:38:37 +0900

    ログイン処理への新データベース導入

--- login.php
+++ login.php
@@ -2,40 +2,51 @@
 <head><title>ログイン</title></head>
 <body>
 <?php
-$name= $_POST['name'];
-$pass = $_POST['pass'];
+    $name= $_POST['name'];
+    $pass = $_POST['pass'];
 
-//DBへ接続
-require "database.php";
+    //DBへ接続
+    //require "database.php";
+    $host = "localhost";
 
-$userSelect = $mysqli->query("select * from user
-where name = '$name' and pass = '$pass'");
+    $mysqli = new mysqli($host, 'root', 's1413109db', 'yuitf2');
+    //エラーが発生したら
+    if ($mysqli->connect_error){
+      print("接続失敗：" . $mysqli->connect_error);
+      exit();
+    }
+    mysql_set_charset("utf8");
 
-if (!$userSelect)
-{
-   print("SQLの実行に失敗しました<BR>");
-   exit;
-}
-else {
-    //データの取り出し
-    $user = $userSelect->fetch_object();
-}
-//ログイン処理
-if(isset($user->name)) {
-    session_start();
-    $userName = $user->name;
-    $userId = $user->userId;
-    $_SESSION['userName'] = $userName;
-    $_SESSION['userId'] = $userId;
-    print('ようこそ'.$userName.'さん<br />');
+    $userSelect = $mysqli->query("select * from users
+    where user_name = '$name' and user_pass = '$pass'");
 
-    print('<a href = "contribute.php">投稿画面へ</a>');
-}
-//エラー表示
-else{
-  print('IDもしくはPassが違います。<br />');
-  print('<a href = "login.html">ログイン画面へ</a>');
-}
+    if (!$userSelect)
+    {
+       print("SQLの実行に失敗しました<BR>");
+       exit;
+    }
+    else {
+        //データの取り出し
+        $user = $userSelect->fetch_object();
+        unset($userSelect);
+    }
+    //ログイン処理
+    if(isset($user->user_id)) {
+        session_start();
+        $userName = $user->user_name;
+        $userId = $user->user_id;
+        $_SESSION['userName'] = $userName;
+        $_SESSION['userId'] = $userId;
+        unset($user);
+        print('ようこそ'.$userName.'さん<br />');
+
+        print('<a href = "contribute.php">投稿画面へ</a>');
+    }
+    //エラー表示
+    else{
+      print('IDもしくはPassが違います。<br />');
+      print('<a href = "login.html">ログイン画面へ</a>');
+    }
 ?>
 
 </body>
